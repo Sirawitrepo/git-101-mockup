7bdb07421c0acf92fa11391ae012de859c71daab
Sirawitrepo <sirawit.heart09@gmail.com>
2023-07-13 00:11:14 +0700

    initial commit

---
+++ mockup_data.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+from pprint import pprint
+
+# data = [10,20,30,40,50,60]
+
+
+
+
+data = {'Name': ['Tom', 'nick', 'krish', 'jack'],
+
+        'Age': [20, 21, 19, 18]}
+
+
+
+
+# df = pd.DataFrame(data, columns=['Numbers'])
+
+
+
+
+df = pd.DataFrame(data)
+
+
+
+
+print(df)
